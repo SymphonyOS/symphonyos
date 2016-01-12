b0174524344191da911e811797b24eee4c092c04
Ryan Quinn <ryan@digitalocean.com>
2016-01-12 13:22:03 -0500

    added build config

--- build/setup.sh
+++ build/setup.sh
@@ -5,20 +5,21 @@ apt-get update;
 apt-get -y upgrade;
 
 # Install necessary packages
-apt-get -y install openbox lightdm pcmanfm tint2 nitrogen build-essential ruby ruby-sinatra ruby-dev libwebkit-dev lxterminal nm-applet volumeicon git;
+apt-get -y install openbox lightdm libffi-dev pcmanfm tint2 nitrogen build-essential ruby ruby-sinatra ruby-dev libwebkit-dev lxterminal git;
 
 # Pull source
 cd /tmp;
 git clone git@github.com:ryanpq/symphonyos.git;
+cp -Rf /tmp/symphonyos/build /tmp/build;
 rm -Rf /tmp/symphonyos/build;
 cp -Rf /tmp/symphonyos/* /.;
 rm -Rf /tmp/symphonyos;
 
 # Build/Install Ruby Gems
 gem install gtk2;
-gem install webkit;
+gem install gtk-webkit-ruby;
 gem install parseconfig;
-gem install pp;
+
 
 # Pull xresprobe (no longer in Ubuntu as of 15.10)
 wget https://launchpad.net/ubuntu/+source/xresprobe/0.4.24ubuntu9/+build/1274262/+files/xresprobe_0.4.24ubuntu9_amd64.deb;
@@ -28,3 +29,5 @@ apt-get -fy install;
 wget http://nyc.symphonyos.com/pinguybuilder_4.3-2_all.deb;
 dpkg -i pinguybuilder_4.3-2_all.deb;
 apt-get -fy install;
+cp /tmp/build/PinguyBuilder.conf /etc/.;
+PinguyBuilder dist;
